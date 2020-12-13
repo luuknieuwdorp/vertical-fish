cfa03d8dd59fbd4cc655d192dd7fc3031c3b70b2
Luuk Nieuwdorp <luuknieuwdorp@gmail.com>
2020-12-13 21:41:18 +0100

    Re-order git prompt before dir prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -15,9 +15,9 @@ function fish_prompt
     # Build the prompt
     __vertical_component_vert
     __vertical_component_jobs
-    __vertical_component_dir
-    __vertical_component_git_branch
     __vertical_component_git_status
+    __vertical_component_git_branch
+    __vertical_component_dir
     __vertical_component_duration
     echo
     __vertical_component_vert
